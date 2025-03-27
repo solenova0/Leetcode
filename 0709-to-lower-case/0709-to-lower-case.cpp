18fbde08198462f49acc1cb1878707413bdf1744
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2025-03-27 14:00:13 +0300

    Time: 0 ms (100%), Space: 8.3 MB (15.07%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.cpp
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -1,9 +1,9 @@
 class Solution {
  public:
   string toLowerCase(string str) {
-     for (auto& c : str) {
-      c = 'A' <= c && 'Z' >= c ? c + ('a' - 'A') : c;
-    }
+     for (char& x : str) { 
+        x = tolower(x); 
+    } 
     return str;
   }
  };
