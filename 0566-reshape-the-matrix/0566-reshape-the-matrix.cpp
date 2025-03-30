708dcfe480dd21c0d9307bcce1de1b8c31d72c6b
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2025-03-30 09:45:56 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.cpp
@@ -0,0 +1,13 @@
+ class Solution {
+ public:
+  vector<vector<int>> matrixReshape(vector<vector<int>>& nums, int r, int c) {
+    if (nums.empty() && nums[0].empty()) return { {} };
+    int m = nums.size(), n = nums[0].size();
+    if (m * n != r * c) return nums;
+    vector<vector<int>> ret(r, vector<int>(c, 0));
+    for (int i = 0; i < r; ++i) {
+      for (int j = 0; j < c; ++j) {
+        int idx = i * c + j;
+        ret[i][j] = nums[idx / n][(idx % n)];
+      }
+    }
