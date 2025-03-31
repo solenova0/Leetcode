1ab5e54f6a410baa6e4ac9999b28321ebdce0f16
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2025-03-31 13:09:16 +0300

    Time: 0 ms (100%), Space: 63.6 MB (48.63%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -0,0 +1,11 @@
+ class Solution {
+ public:
+  int peakIndexInMountainArray(vector<int>& A) {
+    for (int i = 0; i < (int)A.size() - 1; ++i) {
+      if (A[i] > A[i + 1]) {
+        return i;
+      }
+    }
+    return A.size() - 1;
+  }
+ };
