9ce507719684a2bd853dbdeb610eace0041a5d4f
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2025-05-04 12:08:38 +0300

    Time: 15 ms (63.34%), Space: 12.8 MB (73.52%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        left, right = 0, len(s) - 1
+      
+        while left < right:
+            if not s[left].isalnum():
+                left += 1
+            elif not s[right].isalnum():
+                right -= 1
+            elif s[left].lower() != s[right].lower():
+                return False
+            else:
+                left += 1
+                right -= 1
+      
+        # If we haven't found any mismatches, then it's a palindrome.
+        return True
