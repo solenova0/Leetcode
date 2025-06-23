7c5fa887962e7f895a19de336fe36efdfaacf648
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2025-06-23 10:09:35 +0300

    Time: 4 ms (48.61%), Space: 19.1 MB (6.73%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,16 +1,7 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
-        
-        # ensure n is at the last index
-        if nums[-1] !=len(nums):
-          return len(nums)
-        # ensure 0 is at the first index
-        elif nums[0] != 0:
-          return 0
-        
-        # otherwise, the missing number is in the range (0, n)
-        for i in range(1, len(nums)):
-          expected_num = nums[i-1] + 1
-          if nums[i] != expected_num:
-            return expected_num
+        nums_set = set(nums)
+        n = len(nums) + 1
+        for number in range(n):
+            if number not in nums_set:
+                return number
