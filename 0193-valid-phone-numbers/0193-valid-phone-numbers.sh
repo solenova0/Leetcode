601b349628429e28b701afb0bf085b3e1e3e7c50
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2025-06-24 02:40:07 +0300

    Time: 68 ms (32.89%), Space: 3.5 MB (45.25%) - LeetHub

--- 0193-valid-phone-numbers/0193-valid-phone-numbers.sh
+++ 0193-valid-phone-numbers/0193-valid-phone-numbers.sh
@@ -1,2 +1,2 @@
 # Read from the file file.txt and output all valid phone numbers to stdout.
-grep -e "^[0-9]\{3\}\-[0-9]\{3\}\-[0-9]\{4\}$" -e "^([0-9]\{3\}) [0-9]\{3\}\-[0-9]\{4\}$" file.txt
+egrep '^[0-9]{3}-[0-9]{3}-[0-9]{4}$|^\([0-9]{3}\) [0-9]{3}-[0-9]{4}$' file.txt
