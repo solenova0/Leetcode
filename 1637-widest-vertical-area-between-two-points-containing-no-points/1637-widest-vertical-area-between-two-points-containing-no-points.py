4b4db07335b4e2dffd339e45e2b328cd00780606
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2025-07-12 09:53:25 +0300

    Time: 43 ms (82.54%), Space: 52.7 MB (6.84%) - LeetHub

--- 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.py
+++ 1637-widest-vertical-area-between-two-points-containing-no-points/1637-widest-vertical-area-between-two-points-containing-no-points.py
@@ -1,4 +1,9 @@
+from typing import List
+import itertools  # for zip in Python 3
+
 class Solution:
     def maxWidthOfVerticalArea(self, points: List[List[int]]) -> int:
-      sorted_x = sorted({x for x, y in points})
-        return max([b-a for a, b in itertools.izip(sorted_x, sorted_x[1:])] + [0])
+        # Extract unique x-coordinates and sort them
+        sorted_x = sorted({x for x, y in points})
+        # Calculate maximum difference between consecutive x-coordinates
+        return max([b - a for a, b in zip(sorted_x, sorted_x[1:])] + [0])
