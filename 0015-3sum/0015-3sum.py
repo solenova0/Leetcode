84c2ec45c15b433843ee15d1517873310bbe7559
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2025-09-05 18:27:05 +0300

    Time: 403 ms (95.89%), Space: 20.8 MB (36.03%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,25 +1,27 @@
-class Solution(object):
-     def threeSum(self, nums):
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
         res = []
         nums.sort()
-        ls = len(nums)
-        for i in range(ls - 2):
-            if i > 0 and nums[i] == nums[i - 1]:
+
+        for i, a in enumerate(nums):
+            if a > 0:
+                break
+
+            if i > 0 and a == nums[i - 1]:
                 continue
-            j = i + 1
-            k = ls - 1
-            while j < k:
-                curr = nums[i] + nums[j] + nums[k]
-                if curr == 0:
-                    res.append([nums[i], nums[j], nums[k]])
-                    while j < k and nums[j] == nums[j + 1]:
-                        j += 1
-                    while j < k and nums[k] == nums[k - 1]:
-                        k -= 1
-                    j += 1
-                    k -= 1
-                elif curr < 0:
-                    j += 1
+
+            l, r = i + 1, len(nums) - 1
+            while l < r:
+                threeSum = a + nums[l] + nums[r]
+                if threeSum > 0:
+                    r -= 1
+                elif threeSum < 0:
+                    l += 1
                 else:
-                    k -= 1
+                    res.append([a, nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while nums[l] == nums[l - 1] and l < r:
+                        l += 1
+
         return res
