3e2de357063f2b324bbcc8f5f0a519af5f6e8530
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2025-10-06 20:59:18 +0300

    Time: 3 ms (42.1%), Space: 23.2 MB (17.02%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -3,10 +3,10 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        # left = 0
-        # right = len(s) - 1
-        # while left < right:
-        #     s[left],s[right] = s[right],s[left]
-        #     left += 1
-        #     right -= 1
-        s[:] = s[::-1]
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            s[left],s[right] = s[right],s[left]
+            left += 1
+            right -= 1
+        # s[:] = s[::-1]   # Slicing in python
