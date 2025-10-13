e33d6bb874b22a629441064b2b4b00aabdfc3bda
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2025-10-13 14:20:19 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,24 +1,20 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        n1, n2 = len(s1), len(s2)
-        if n1 > n2:
+        class Solution:
+        len1, len2 = len(s1), len(s2)
+        if len1 > len2:
             return False
         
-        count1 = Counter(s1)
-        count2 = Counter(s2[:n1])            #Time : O(n2)
-                                             #Space : O(25) + O(25) = O(1) - Constant
-        if count1 == count2:
-            return True
+        count1 = [0] * 26
+        count2 = [0] * 26
+        for i in range(len1):
+            count1[ord(s1[i]) - ord('a')] += 1
+            count2[ord(s2[i]) - ord('a')] += 1
         
-        for i in range(n1, n2):
-            count2[s2[i]] += 1
-            left_char = s2[i - n1]
-            count2[left_char] -= 1
-            if count2[left_char] == 0:
-                del count2[left_char]
+        for i in range(len2 - len1):
             if count1 == count2:
                 return True
+            count2[ord(s2[i + len1]) - ord('a')] += 1
+            count2[ord(s2[i]) - ord('a')] -= 1
         
-        return False
-
-        
+        return count1 == count2
