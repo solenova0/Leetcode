f8d011748a76ea826e8e62edcb15651232928515
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2025-10-13 05:57:05 +0300

    Time: 3 ms (79.77%), Space: 17.8 MB (39.89%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,17 +1,34 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        total = 0
-        i = 0
-        total = 0
+        # roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        # total = 0
+        # i = 0
+        # total = 0
 
-        while i < len(s):
-            if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
-               total += roman[s[i + 1]] - roman[s[i]]
-               i += 2   
-            else:
-               total += roman[s[i]]
-               i += 1    
-        return total
+        # while i < len(s):
+        #     if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+        #        total += roman[s[i + 1]] - roman[s[i]]
+        #        i += 2   
+        #     else:
+        #        total += roman[s[i]]
+        #        i += 1    
+        # return total
 
         
+        roman = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        for i in range(len(s)):
+            if i + 1 < len(s) and roman[s[i]] < roman[s[i + 1]]:
+                total -= roman[s[i]]
+            else:
+                total += roman[s[i]]
+        return total
