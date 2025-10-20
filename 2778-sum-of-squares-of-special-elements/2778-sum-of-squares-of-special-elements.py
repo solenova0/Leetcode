53357e8c5fcbda9bd67e466de340edf55f3a6942
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2025-10-20 14:32:11 +0300

    Time: 5 ms (5.32%), Space: 17.9 MB (25.53%) - LeetHub

---
+++ 2778-sum-of-squares-of-special-elements/2778-sum-of-squares-of-special-elements.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sumOfSquares(self, nums: List[int]) -> int:
+        n = len(nums)
+        sum = 0
+        for i in range(n):
+            if n % (i + 1) == 0:
+                sum += nums[i] * nums[i]
+        return sum
