d318525e70169dd99ca095ba9f52f24fcf7ef4cf
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2025-11-12 08:42:06 +0300

    Time: 19 ms (54.34%), Space: 17.7 MB (81.76%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,23 +1,23 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # l = 0
-        # curr_longest = 0
-        # set1 = set()
-        # for r in range(len(s)):
-        #     while s[r] in set1:
-        #         set1.remove(s[l])
-        #         l += 1
-        
-        #     set1.add(s[r])
-        #     curr_longest = max(curr_longest, r - l + 1)
-        # return curr_longest
-
-        dic = {}
         l = 0
-        longest = 0
+        curr_longest = 0
+        set1 = set()
         for r in range(len(s)):
-            if s[r] in dic and dic[s[r]] >= l:
-                l = dic[s[r]] + 1
-            dic[s[r]] = r
-            longest = max(longest, r - l +1)  
-        return longest 
+            while s[r] in set1:
+                set1.remove(s[l])
+                l += 1
+        
+            set1.add(s[r])
+            curr_longest = max(curr_longest, r - l + 1)
+        return curr_longest
+
+        # dic = {}
+        # l = 0
+        # longest = 0
+        # for r in range(len(s)):
+        #     if s[r] in dic and dic[s[r]] >= l:
+        #         l = dic[s[r]] + 1
+        #     dic[s[r]] = r
+        #     longest = max(longest, r - l +1)  
+        # return longest 
