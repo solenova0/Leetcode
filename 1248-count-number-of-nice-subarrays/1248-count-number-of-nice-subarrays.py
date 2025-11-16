c7e00d58583fbf8de5274f2b74e8d497ddb14c99
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2025-11-16 23:02:22 +0300

    Time: 99 ms (63.29%), Space: 26.8 MB (9.6%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -1,12 +1,14 @@
-from typing import List
-from collections import Counter
-
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        cnt = Counter({0: 1})  # Stores count of prefix sums
-        ans = t = 0  # t = number of odd numbers seen so far
-        for v in nums:
-            t += v % 2  # +1 if odd, +0 if even
-            ans += cnt[t - k]  # Add number of times (t - k) has occurred
-            cnt[t] += 1  # Record current number of odd numbers
+        freq = defaultdict(int)
+        freq[0] = 1
+
+        curr = 0
+        ans = 0
+
+        for x in nums:
+            curr += x % 2 
+            ans += freq[curr - k]
+            freq[curr] += 1
+
         return ans
