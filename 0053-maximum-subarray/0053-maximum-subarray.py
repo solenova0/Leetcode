640d827e7c8e8ffde63ff0e272f7cc0fca0abfac
Solan Adugna <149063873+solenova0@users.noreply.github.com>
2025-12-03 17:13:08 +0300

    Time: 43 ms (87.03%), Space: 31.9 MB (82.56%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -10,18 +10,15 @@ class Solution:
         #         curr_sum = 0
         # return max_sum
         max_sum = current_sum = nums[0]
-        start = end = temp_start = 0
         
         for i in range(1, len(nums)):
             if nums[i] > current_sum + nums[i]:
                 current_sum = nums[i]
-                temp_start = i
             else:
                 current_sum += nums[i]
             
             if current_sum > max_sum:
                 max_sum = current_sum
-                start = temp_start
-                end = i
+                
         
         return max_sum
